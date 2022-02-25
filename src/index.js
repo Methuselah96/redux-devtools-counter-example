b9f1115c0dcd7bc663aea8d4576700892d21cae3
Nathan Bierema <nbierema@gmail.com>
2022-02-25 10:40:50 -0500

    Add extension

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { createStore } from 'redux'
+import { devToolsEnhancer } from '@redux-devtools/extension';
 import Counter from './components/Counter'
 import counter from './reducers'
 
-const store = createStore(counter)
+const store = createStore(counter, devToolsEnhancer())
 const rootEl = document.getElementById('root')
 
 const render = () => ReactDOM.render(
